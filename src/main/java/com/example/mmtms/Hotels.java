7e14c715406e4025f6afba8dcffe79378d4cc84c
kedar <kedarkapratwar@gmail.com>
2024-03-18 22:39:09 +0530

    added new module and created package and tsted

--- src/main/java/com/example/mmtms/Hotels.java
+++ src/main/java/com/example/mmtms/Hotels.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-public class Train {
-    @GetMapping("/hotelsn")
+public class Hotels {
+    @GetMapping("/hotels")
     public String getData() {return "Please book a Hotel reservations at 25% discount";}
 }
