ee6cb7fa82c5b6c952055c2bf9c1e51712f3701d
kedar <kedarkapratwar@gmail.com>
2024-03-18 23:08:46 +0530

    added Hotels module, compiled and packaged

---
+++ src/main/java/com/example/mmtms/Hotels.java
@@ -0,0 +1,11 @@
+package com.example.mmtms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class Hotels {
+    @GetMapping("/hotels")
+    public String getData() {return "Please do the reservationgs at 20% discount";}
+}
